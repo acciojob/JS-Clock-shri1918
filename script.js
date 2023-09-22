c8289b7d854bf767b6f07099f9dbb05b9213d4c8
Shridhar Savant <93203347+shri1918@users.noreply.github.com>
2023-09-22 12:05:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,23 @@
-// Get references to the clock hands
-const hourHand = document.querySelector('.hour-hand');
-const minHand = document.querySelector('.min-hand');
-const secondHand = document.querySelector('.second-hand');
-
 function updateClock() {
-  const now = new Date();
+  var now = new Date();
+  var hour = now.getHours();
+  var minute = now.getMinutes();
+  var second = now.getSeconds();
 
-  // Get the current time in hours, minutes, and seconds
-  const hours = now.getHours() % 12; // 12-hour format
-  const minutes = now.getMinutes();
-  const seconds = now.getSeconds();
+  var hourHand = document.getElementByClassName('hand hour-hand');
+  var minHand = document.getElementByClassName('hand min-hand');
+  var secondHand = document.getElementByClassName('hand second-hand');
 
-  // Calculate the angles for the clock hands
-  const hourAngle = (360 / 12) * hours + (360 / 12) * (minutes / 60);
-  const minAngle = (360 / 60) * minutes + (360 / 60) * (seconds / 60);
-  const secondAngle = (360 / 60) * seconds;
+  // Calculate the rotation angles for the clock hands
+  var hourRotation = 30 * (hour % 12) + minute / 2; // Each hour is 30 degrees, and each minute adds 0.5 degrees
+  var minuteRotation = 6 * minute + second / 10; // Each minute is 6 degrees, and each second adds 0.1 degrees
+  var secondRotation = 6 * second; // Each second is 6 degrees
 
-  // Apply rotations to the clock hands
-  hourHand.style.transform = `rotate(${hourAngle}deg)`;
-  minHand.style.transform = `rotate(${minAngle}deg)`;
-  secondHand.style.transform = `rotate(${secondAngle}deg)`;
+  // Apply the rotation using CSS transform
+  hourHand.style.transform = 'rotate(' + hourRotation + 'deg)';
+  minuteHand.style.transform = 'rotate(' + minuteRotation + 'deg)';
+  secondHand.style.transform = 'rotate(' + secondRotation + 'deg)';
 }
 
-// Update the clock every second
+// Call the updateClock function every second to keep the clock hands updated
 setInterval(updateClock, 1000);
-
-// Initial call to set the initial clock state
-updateClock();
