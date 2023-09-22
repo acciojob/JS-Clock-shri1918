690d53120b6327fc4c20502c5d1b5b0bc0c145ed
Shridhar Savant <93203347+shri1918@users.noreply.github.com>
2023-09-22 11:56:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-//your code here
+// Get references to the clock hands
+const hourHand = document.querySelector('.hour-hand');
+const minHand = document.querySelector('.min-hand');
+const secondHand = document.querySelector('.second-hand');
+
 function updateClock() {
-    const clock = document.querySelector('.clock');
-    const hourHand = document.querySelector('.hour-hand');
-    const minuteHand = document.querySelector('.minute-hand');
-    const secondHand = document.querySelector('.second-hand');
+  const now = new Date();
 
-    const now = new Date();
-    const hours = now.getHours() % 12; // Convert to 12-hour format
-    const minutes = now.getMinutes();
-    const seconds = now.getSeconds();
+  // Get the current time in hours, minutes, and seconds
+  const hours = now.getHours() % 12; // 12-hour format
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
 
-    const hourDeg = (360 / 12) * hours + (360 / 12) * (minutes / 60);
-    const minuteDeg = (360 / 60) * minutes + (360 / 60) * (seconds / 60);
-    const secondDeg = (360 / 60) * seconds;
+  // Calculate the angles for the clock hands
+  const hourAngle = (360 / 12) * hours + (360 / 12) * (minutes / 60);
+  const minAngle = (360 / 60) * minutes + (360 / 60) * (seconds / 60);
+  const secondAngle = (360 / 60) * seconds;
 
-    hourHand.style.transform = `rotate(${hourDeg}deg)`;
-    minuteHand.style.transform = `rotate(${minuteDeg}deg)`;
-    secondHand.style.transform = `rotate(${secondDeg}deg)`;
+  // Apply rotations to the clock hands
+  hourHand.style.transform = `rotate(${hourAngle}deg)`;
+  minHand.style.transform = `rotate(${minAngle}deg)`;
+  secondHand.style.transform = `rotate(${secondAngle}deg)`;
 }
 
 // Update the clock every second
 setInterval(updateClock, 1000);
 
-// Initial update
+// Initial call to set the initial clock state
 updateClock();
