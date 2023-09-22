9eea528e68a607b1a8de355282d73f2ff22d319f
Shridhar Savant <93203347+shri1918@users.noreply.github.com>
2023-09-22 11:53:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your code here
+function updateClock() {
+    const clock = document.querySelector('.clock');
+    const hourHand = document.querySelector('.hour-hand');
+    const minuteHand = document.querySelector('.minute-hand');
+    const secondHand = document.querySelector('.second-hand');
+
+    const now = new Date();
+    const hours = now.getHours() % 12; // Convert to 12-hour format
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+
+    const hourDeg = (360 / 12) * hours + (360 / 12) * (minutes / 60);
+    const minuteDeg = (360 / 60) * minutes + (360 / 60) * (seconds / 60);
+    const secondDeg = (360 / 60) * seconds;
+
+    hourHand.style.transform = `rotate(${hourDeg}deg)`;
+    minuteHand.style.transform = `rotate(${minuteDeg}deg)`;
+    secondHand.style.transform = `rotate(${secondDeg}deg)`;
+}
+
+// Update the clock every second
+setInterval(updateClock, 1000);
+
+// Initial update
+updateClock();
